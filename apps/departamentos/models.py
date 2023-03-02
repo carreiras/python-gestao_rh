e8de11e2bb56fa464aa8b7d898b7536e2884707a
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-03-02 18:35:22 -0300

    Preparando o sistema de templates

--- apps/departamentos/models.py
+++ apps/departamentos/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Departamento(models.Model):
     nome = models.CharField(max_length=70)
--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from apps.funcionarios.models import Funcionario
 
-# Create your models here.
-
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
